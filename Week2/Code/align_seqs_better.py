db7ee0238c335762bd43b4968754755156df3b64
RLBat <Rachel.Bates@outlook.com>
2018-10-17 16:20:00 +0100

    Commented align_seqs_fasta and commented and finished align_seqs_better

--- Week2/Code/align_seqs_better.py
+++ Week2/Code/align_seqs_better.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
-""" Compares two base sequences and finds the position at which they most closely match """
+""" Compares two DNA base sequences and finds the position at which they most 
+    closely match. Where there are multiple best matches, it records all of them.
+    The best matches are outputted to a file in ../Output/ """
 
 __author__ = 'Rachel Bates r.bates18@imperial.ac.uk'
 __version__ = '0.0.1'
 
 ## Imports ##
 
-import pandas
+import pandas # Imports pandas, a library that interacts with csvs
 
 ## Constants ##
 
@@ -20,13 +22,13 @@ seq2 = seqs.loc[0][1]
 
 ## Functions ##
 
-# A function that computes a score by returning the number of matches starting
-# from arbitrary startpoint (chosen by user)
 def calculate_score(s1, s2, l1, l2, startpoint):
+    """ Computes a score by returning the number of matches between two sequences
+    starting an from arbitrary startpoint (chosen by user)"""
     matched = "" # to hold string displaying alignements
     score = 0
-    for i in range(l2):
-        if (i + startpoint) < l1:
+    for i in range(l2): # Runs the loop as many times as the l2
+        if (i + startpoint) < l1: # Checks to see if l1 has been exceeded
             if s1[i + startpoint] == s2[i]: # if the bases match
                 matched = matched + "*"
                 score = score + 1
@@ -60,12 +62,19 @@ else:
 # now try to find the best match (highest score) for the two sequences
 my_best_align = None
 my_best_score = -1
+best_scores = dict() # Creates an empty dictionary
 
-for i in range(l1): # Note that you just take the first alignment with the highest score
+for i in range(l1):
     z = calculate_score(s1, s2, l1, l2, i)
-    if z > my_best_score:
-        my_best_align = "." * i + s2 
+    if z >= my_best_score:
+        my_best_align = "." * i + s2 # Makes a visual representation of match position
         my_best_score = z 
+        # Best score plus its allignment is added to the dictionary
+        best_scores.setdefault(my_best_score, []).append(i)
+
+with open ('../Output/Seq_matches.txt', 'w') as data:
+    data.write(str(best_scores)) # Writes the best_scores dictionary to a text file
+
 print(my_best_align)
 print(s1)
 print("Best score:", my_best_score)
--- Week2/Code/align_seqs_fasta.py
+++ Week2/Code/align_seqs_fasta.py
@@ -13,56 +13,52 @@ import sys
 
 ## Constants ##
 
-if len(sys.argv) == 3:
-    with open (sys.argv[1]) as f:
+if len(sys.argv) == 3: # If user has entered two files
+    with open (sys.argv[1]) as f: # Opens first file as a list\
+        #and removes newline characters
         seq1 = [line.rstrip('\n') for line in f]
     
-    seq1[0:len(seq1)] = [''.join(seq1[0:len(seq1)])]
-    seq1 = seq1[0]
+    seq1[0:len(seq1)] = [''.join(seq1[0:len(seq1)])] # Merges all list items
+    seq1 = seq1[0] # Makes an object be just the string in the list
 
-    with open (sys.argv[2]) as g:
+    with open (sys.argv[2]) as g:# Opens second file as a list\
+        #and removes newline characters
         seq2 = [line.rstrip('\n') for line in g]
 
-    seq2[0:len(seq2)] = [''.join(seq2[0:len(seq2)])]
-    seq2 = seq2[0]
-    #file1 = open(sys.argv[1], 'r')
-    #file2 = open(sys.argv[2], "r")
-else:
-    with open ('../Data/407228326.fasta') as f:
+    seq2[0:len(seq2)] = [''.join(seq2[0:len(seq2)])] # Merges all list items
+    seq2 = seq2[0]# Makes an object be just the string in the list
+
+else: #If no user inputs or incorrect number of inputs
+    with open ('../Data/407228326.fasta') as f:# Opens first default file as a \
+        #list and removes newline characters
         seq1 = [line.rstrip('\n') for line in f]
     
-    seq1[0:len(seq1)] = [''.join(seq1[0:len(seq1)])]
-    seq1 = seq1[0]
+    seq1[0:len(seq1)] = [''.join(seq1[0:len(seq1)])] # Merges all list items
+    seq1 = seq1[0]# Makes an object be just the string in the list
 
-    with open ('../Data/407228412.fasta') as g:
+    with open ('../Data/407228412.fasta') as g:# Opens second default file as a\ 
+        #list and removes newline characters
         seq2 = [line.rstrip('\n') for line in g]
 
-    seq2[0:len(seq2)] = [''.join(seq2[0:len(seq2)])]
-    seq2 = seq2[0]
+    seq2[0:len(seq2)] = [''.join(seq2[0:len(seq2)])] # Merges all list items
+    seq2 = seq2[0]# Makes an object be just the string in the list
 
 
 
 ## Functions ##
 
-# A function that computes a score by returning the number of matches starting
-# from arbitrary startpoint (chosen by user)
 def calculate_score(s1, s2, l1, l2, startpoint):
+    """ Computes a score by returning the number of matches between two sequences
+    starting an from arbitrary startpoint (chosen by user)"""
     matched = "" # to hold string displaying alignements
     score = 0
-    for i in range(l2):
-        if (i + startpoint) < l1:
+    for i in range(l2): # Runs the loop as many times as the l2
+        if (i + startpoint) < l1: # Checks to see if l1 has been exceeded
             if s1[i + startpoint] == s2[i]: # if the bases match
                 matched = matched + "*"
                 score = score + 1
             else:
                 matched = matched + "-"
-
-    # some formatted output
-    #print("." * startpoint + matched)           
-    #print("." * startpoint + s2)
-    #print(s1)
-    #print(score) 
-    #print(" ")
 
     return score
 
@@ -87,8 +83,8 @@ my_best_score = -1
 
 for i in range(l1): # Note that you just take the first alignment with the highest score
     z = calculate_score(s1, s2, l1, l2, i)
-    if z > my_best_score:
-        my_best_align = "." * i + s2 
+    if z > my_best_score: # Only runs if the best score is exceeded
+        my_best_align = "." * i + s2 # Makes a visual representation of match position
         my_best_score = z 
 print(my_best_align)
 print(s1)
