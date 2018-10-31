3d47d5e9aa64ec336a6be8078b46029a9b11cd1a
RLBat <Rachel.Bates@outlook.com>
2018-10-31 15:00:36 +0000

    Added Girko and Mybars inc pdf output

---
+++ Week3/Code/MyBars.R
@@ -0,0 +1,73 @@
+## 
+
+# Author: Rachel Bates (r.bates18@imperial.ac.uk)
+# Version: 0.0.1
+
+## Clear the directory ##
+
+rm(list=ls())
+
+## Packages ##
+
+require(ggplot2)
+
+## Load Data ##
+
+a <- read.table("../Data/Results.txt", header = TRUE)
+
+###############
+
+
+
+
+
+a$ymin <- rep(0, dim(a)[1]) # append a column of zeros
+
+# Print the first linerange
+p <- ggplot(a)
+p <- p + geom_linerange(data = a, aes(
+                          x = x,
+                          ymin = ymin,
+                          ymax = y1,
+                          size = (0.5)
+                          ),
+                        colour = "#E69F00",
+                        alpha = 1/2, show.legend = FALSE)
+
+# Print the second linerange
+p <- p + geom_linerange(data = a, aes(
+                          x = x,
+                          ymin = ymin,
+                          ymax = y2,
+                          size = (0.5)
+                          ),
+                        colour = "#56B4E9",
+                        alpha = 1/2, show.legend = FALSE)
+
+# Print the third linerange:
+p <- p + geom_linerange(data = a, aes(
+                          x = x,
+                          ymin = ymin,
+                          ymax = y3,
+                          size = (0.5)
+                          ),
+                        colour = "#D55E00",
+                        alpha = 1/2, show.legend = FALSE)
+
+# Annotate the plot with labels:
+p <- p + geom_text(data = a, aes(x = x, y = -500, label = Label))
+
+# now set the axis labels, remove the legend, and prepare for bw printing
+p <- p + scale_x_continuous("My x axis",
+                            breaks = seq(3, 5, by = 0.05)) + 
+                            scale_y_continuous("My y axis") + 
+                            theme_bw() + 
+                            theme(legend.position = "none") 
+
+## SAVE OUTPUT TO PDF
+
+pdf("../Output/MyBars.pdf", # Open blank pdf page using a relative path
+    9, 11.7)
+# Prints the created ggplot to the pdf
+print(p)
+graphics.off()
